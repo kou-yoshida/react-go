bbbbbd21290fe577fb231827f6c5e054c005fb8e
kou-yoshida <kouyoshida28@gmail.com>
2024-08-21 23:31:54 +0900

    🚧 チュートリアル途中まで

--- main.go
+++ main.go
@@ -7,17 +7,50 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+
+type Todo struct {
+	ID        int `json:"id"`
+	Completed bool `json:"completed"`
+	Body      string `json:"body"`
+}
+
 func main(){
-	fmt.Println("hello world!!!!")
+	fmt.Println("hello world!!")
 
 	app := fiber.New()
 
 
+	todos := []Todo{}
+
 	app.Get("/", func(c *fiber.Ctx) error {
-		return c.Status(200).JSON(fiber.Map{"msg":"Hello, World!"})	
-
-
+		fmt.Println(Todo{})
+		fmt.Println("requestが来た")
+		return c.Status(202).JSON(fiber.Map{"msg": "Hello, World"})
 	})
 
+	app.Post("/todos", func(c *fiber.Ctx) error {
+
+		todo := &Todo{}
+
+
+		if err := c.BodyParser(todo); err != nil {
+			fmt.Println(err)
+			return err
+		}
+
+		if todo.Body == ""{
+			return c.Status(400).JSON(fiber.Map{"error": "Body is empty"})
+		}
+
+		todo.ID = len(todos) + 1
+		todos = append(todos, *todo)
+
+		return c.Status(201).JSON(todo)
+	})
+
+
+
+	
 	log.Fatal(app.Listen(":4000"))
+
 }
